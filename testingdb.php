22fdeefdadef7581baa1b9447b39537a36b9553b
MattCarter94 <Matt_carter18@live.co.uk>
2017-11-04 21:21:42 +0000

    added basic database connection test file

---
+++ testingdb.php
@@ -0,0 +1,29 @@
+<html>
+    <head>
+        <title>Gift.Me</title>
+        <!--Stylesheets, JavaScript etc here-->
+    </head>
+    <body>
+        
+       <?php include "dbconnect.inc" ;
+        $sql = "SELECT * FROM users";
+        $result = $connect->query($sql);
+        
+        if ($result->num_rows > 0) {
+        // output data of each row
+            while($row = $result->fetch_assoc()) {
+                echo "Username: " . $row['username'] . " | First Name: " . $row['first_name'] . " | Surname: " . $row['surname'] . "<br>";
+            }
+        } else {
+            echo "0 results";
+        }
+        
+        
+        ?>
+        
+        
+        
+        
+        
+    </body>
+</html>
