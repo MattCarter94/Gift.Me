e658b79b3e6120b9a98ed051c47a2352137147f0
MattCarter94 <Matt_carter18@live.co.uk>
2017-11-04 20:46:56 +0000

    basic layout of index.php file implemented

--- index.php
+++ index.php
@@ -1,6 +1,16 @@
-<?php
-
-echo "Hi!";
-echo "Matt's a bitch!";
-
-?>
+<html>
+    <head>
+        <title>Gift.Me</title>
+        <!--Stylesheets, JavaScript etc here-->
+    </head>
+    <body>
+        
+        <p>This is text rendered in HTML</p>
+        <?php echo "this is text rendered on the server, by the PHP engine"; ?>
+        <p>I am text rendered in HTML, but <b> <?php echo"I am text rendered in PHP!"; ?></b> </p>
+        
+        
+        
+        
+    </body>
+</html>
