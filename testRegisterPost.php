db788c094e6692bbed2d0590337d73456c1bb55a
Morshead <tm1993@hotmail.co.uk>
2017-11-10 16:30:09 +0000

    updated

--- testRegisterPost.php
+++ testRegisterPost.php
@@ -7,12 +7,13 @@
     /*** connect to database ***/
     include "dbconnect.inc" ;
 
+    /*** don't need to check twice, mysql does it already
     $query = "SELECT * FROM users WHERE username='$username'";
     $result = $connect->query($query);
     if ($result->num_rows > 0) {
         echo 'Sorry, this username already exists!';
         header('Location: testRegister.php?user=exists');
-    }
+    } ***/
     /*** set up what needs to be inserted into the database ***/
     $sql = "INSERT INTO users (first_name, last_name, username, password)
             VALUES ('$firstname', '$lastname', '$username', '$password')";
